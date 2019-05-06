528c7a57e09d379ba548930965b3c2f9e6579d84
Jobey1Kinobi <jobers92@gmail.com>
2019-05-05 22:27:11 -0500

    a

--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000;
-const koalaRouter = require('./routes/koala.router')
+const PORT = 5000;
 
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
 // ROUTES
